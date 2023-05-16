0c94c740faa25d55f0a238f8835bd02c0e423001
Kang Wang <kangqiwang@outlook.com>
2023-05-16 02:50:47 +0100

    buil a structure

---
+++ src/test/test_stock.py
@@ -0,0 +1,15 @@
+import unittest
+from datetime import datetime
+from src.stock import Stock
+class StockTest(unittest.TestCase):
+    def test_price_of_a_new_stock_class_should_be_None(self):
+        stock = Stock("GOOG")
+        self.assertIsNone(stock.price)
+
+    def test_stock_update(self):
+        """
+        
+        """
+        stock = Stock("GOOG")
+        stock.update(datetime(2014, 2, 12), price=10)
+        self.assertEqual(10, stock.price)
